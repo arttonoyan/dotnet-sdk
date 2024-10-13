fc8dcab2104e9484c58b903b9229bd26220938e4
Artyom Tonoyan <artonoyan@servicetitan.com>
2024-10-13 20:10:58 +0400

    test: Improve OpenFeatureBuilderExtensionsTests for better coverage and clarity

--- test/OpenFeature.DependencyInjection.Tests/OpenFeatureBuilderExtensionsTests.cs
+++ test/OpenFeature.DependencyInjection.Tests/OpenFeatureBuilderExtensionsTests.cs
@@ -7,23 +7,29 @@ namespace OpenFeature.DependencyInjection.Tests;
 
 public partial class OpenFeatureBuilderExtensionsTests
 {
+    private readonly IServiceCollection _services;
+    private readonly OpenFeatureBuilder _systemUnderTest;
+
+    public OpenFeatureBuilderExtensionsTests()
+    {
+        _services = new ServiceCollection();
+        _systemUnderTest = new OpenFeatureBuilder(_services);
+    }
+
     [Theory]
     [InlineData(true)]
     [InlineData(false)]
     public void AddContext_Delegate_ShouldAddServiceToCollection(bool useServiceProviderDelegate)
     {
-        // Arrange
-        var services = new ServiceCollection();
-        var builder = new OpenFeatureBuilder(services);
-
         // Act
         var result = useServiceProviderDelegate ?
-            builder.AddContext(_ => { }) :
-            builder.AddContext((_, _) => { });
+            _systemUnderTest.AddContext(_ => { }) :
+            _systemUnderTest.AddContext((_, _) => { });
 
         // Assert
-        result.Should().BeSameAs(builder, "The method should return the same builder instance.");
-        services.Should().ContainSingle(serviceDescriptor =>
+        result.Should().BeSameAs(_systemUnderTest, "The method should return the same builder instance.");
+        _systemUnderTest.IsContextConfigured.Should().BeTrue("The context should be configured.");
+        _services.Should().ContainSingle(serviceDescriptor =>
             serviceDescriptor.ServiceType == typeof(EvaluationContext) &&
             serviceDescriptor.Lifetime == ServiceLifetime.Transient,
             "A transient service of type EvaluationContext should be added.");
@@ -35,20 +41,19 @@ public partial class OpenFeatureBuilderExtensionsTests
     public void AddContext_Delegate_ShouldCorrectlyHandles(bool useServiceProviderDelegate)
     {
         // Arrange
-        var services = new ServiceCollection();
-        var builder = new OpenFeatureBuilder(services);
         bool delegateCalled = false;
 
         _ = useServiceProviderDelegate ?
-            builder.AddContext(_ => delegateCalled = true) :
-            builder.AddContext((_, _) => delegateCalled = true);
+            _systemUnderTest.AddContext(_ => delegateCalled = true) :
+            _systemUnderTest.AddContext((_, _) => delegateCalled = true);
 
-        var serviceProvider = services.BuildServiceProvider();
+        var serviceProvider = _services.BuildServiceProvider();
 
         // Act
         var context = serviceProvider.GetService<EvaluationContext>();
 
         // Assert
+        _systemUnderTest.IsContextConfigured.Should().BeTrue("The context should be configured.");
         context.Should().NotBeNull("The EvaluationContext should be resolvable.");
         delegateCalled.Should().BeTrue("The delegate should be invoked.");
     }
@@ -56,16 +61,13 @@ public partial class OpenFeatureBuilderExtensionsTests
     [Fact]
     public void AddProvider_ShouldAddProviderToCollection()
     {
-        // Arrange
-        var services = new ServiceCollection();
-        var builder = new OpenFeatureBuilder(services);
-
         // Act
-        var result = builder.AddProvider(_ => new NoOpFeatureProvider());
+        var result = _systemUnderTest.AddProvider(_ => new NoOpFeatureProvider());
 
         // Assert
-        result.Should().BeSameAs(builder, "The method should return the same builder instance.");
-        services.Should().ContainSingle(serviceDescriptor =>
+        _systemUnderTest.IsContextConfigured.Should().BeFalse("The context should not be configured.");
+        result.Should().BeSameAs(_systemUnderTest, "The method should return the same builder instance.");
+        _services.Should().ContainSingle(serviceDescriptor =>
             serviceDescriptor.ServiceType == typeof(FeatureProvider) &&
             serviceDescriptor.Lifetime == ServiceLifetime.Singleton,
             "A singleton service of type FeatureProvider should be added.");
@@ -75,16 +77,15 @@ public partial class OpenFeatureBuilderExtensionsTests
     public void AddProvider_ShouldResolveCorrectProvider()
     {
         // Arrange
-        var services = new ServiceCollection();
-        var builder = new OpenFeatureBuilder(services);
-        builder.AddProvider(_ => new NoOpFeatureProvider());
+        _systemUnderTest.AddProvider(_ => new NoOpFeatureProvider());
 
-        var serviceProvider = services.BuildServiceProvider();
+        var serviceProvider = _services.BuildServiceProvider();
 
         // Act
         var provider = serviceProvider.GetService<FeatureProvider>();
 
         // Assert
+        _systemUnderTest.IsContextConfigured.Should().BeFalse("The context should not be configured.");
         provider.Should().NotBeNull("The FeatureProvider should be resolvable.");
         provider.Should().BeOfType<NoOpFeatureProvider>("The resolved provider should be of type DefaultFeatureProvider.");
     }

fe5e072365063167e92baca2ff5d5f984ae4f6a0
Artyom Tonoyan <artonoyan@servicetitan.com>
2024-10-21 10:33:35 +0400

    refactor: Update ThrowIfNull method

--- src/OpenFeature.DependencyInjection/MultiTarget/Guard.cs
+++ src/OpenFeature.DependencyInjection/MultiTarget/Guard.cs
@@ -6,15 +6,9 @@ namespace OpenFeature;
 [DebuggerStepThrough]
 internal static class Guard
 {
-    public static T ThrowIfNull<T>(T? value, [CallerArgumentExpression("value")] string name = null!)
+    public static void ThrowIfNull(object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
     {
-#if NET8_0_OR_GREATER
-        ArgumentNullException.ThrowIfNull(value, name);
-#else
-        if (value is null)
-            throw new ArgumentNullException(name);
-#endif
-
-        return value;
+        if (argument is null)
+            throw new ArgumentNullException(paramName);
     }
 }

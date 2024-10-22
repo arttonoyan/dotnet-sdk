10ea9f11b6eb68e4e2ec262b7daa4692d9cfcc61
Artyom Tonoyan <artonoyan@servicetitan.com>
2024-10-23 01:29:14 +0400

    refactor: Change OpenFeatureBuilder to class and make IsContextConfigured public

--- src/OpenFeature.DependencyInjection/OpenFeatureBuilder.cs
+++ src/OpenFeature.DependencyInjection/OpenFeatureBuilder.cs
@@ -5,13 +5,16 @@ namespace OpenFeature;
 /// <summary>
 /// Describes a <see cref="OpenFeatureBuilder"/> backed by an <see cref="IServiceCollection"/>.
 /// </summary>
-/// <param name="Services">The <see cref="IServiceCollection"/> instance.</param>
-public sealed record OpenFeatureBuilder(IServiceCollection Services)
+/// <param name="services">The services being configured.</param>
+public class OpenFeatureBuilder(IServiceCollection services)
 {
+    /// <summary> The services being configured. </summary>
+    public IServiceCollection Services { get; } = services;
+
     /// <summary>
     /// Indicates whether the evaluation context has been configured.
     /// This property is used to determine if specific configurations or services
     /// should be initialized based on the presence of an evaluation context.
     /// </summary>
-    internal bool IsContextConfigured { get; set; }
+    public bool IsContextConfigured { get; internal set; }
 }

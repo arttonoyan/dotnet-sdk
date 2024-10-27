e8350d7043ec9579d6e3fce4dab3de719877d23c
Artyom Tonoyan <artonoyan@servicetitan.com>
2024-10-27 15:33:28 +0400

    fix(build): Resolve build error in DependencyInjection namespace

--- src/OpenFeature.Hosting/HostedFeatureLifecycleService.cs
+++ src/OpenFeature.Hosting/HostedFeatureLifecycleService.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OpenFeature.DependencyInjection;
 
 namespace OpenFeature;
 
--- src/OpenFeature.Hosting/OpenFeatureBuilderExtensions.cs
+++ src/OpenFeature.Hosting/OpenFeatureBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using OpenFeature.DependencyInjection;
 
 namespace OpenFeature;
 

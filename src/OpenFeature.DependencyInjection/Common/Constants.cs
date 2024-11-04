698ceb0b006e2509912eb6f8ef6c035396ad158f
Artyom Tonoyan <artonoyan@servicetitan.com>
2024-11-04 11:43:51 +0400

    feat(attributes): Add Experimental attribute for .NET 8 and greater DI features

---
+++ src/OpenFeature.DependencyInjection/Common/Constants.cs
@@ -0,0 +1,12 @@
+namespace OpenFeature.DependencyInjection.Common;
+
+/// <summary>
+/// Contains constants for assembly and feature identification.
+/// </summary>
+public static class Constants
+{
+    /// <summary>
+    /// Identifier for the new Dependency Injection features in experimental builds.
+    /// </summary>
+    public const string DIFeatureIdentifier = "NewDiFeatures";
+}
---
+++ src/OpenFeature.DependencyInjection/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+#if NET8_0_OR_GREATER
+    [assembly: System.Diagnostics.CodeAnalysis.Experimental(OpenFeature.DependencyInjection.Common.Constants.DIFeatureIdentifier)]
+#endif
